f8193d7b80b588c0cc9448c2c9985278e1b8d896
Tim Ruthersby <hockeytim11@gmail.com>
2022-08-20 19:25:58 -0600

    Initial commit. Basic working app

---
+++ TimeclockTracker/Punchcard.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace TimeclockTracker
+{
+    partial class Punchcard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.facultyId = new System.Windows.Forms.TextBox();
+            this.clockIn = new System.Windows.Forms.Button();
+            this.clockOut = new System.Windows.Forms.Button();
+            this.printLog = new System.Windows.Forms.ListBox();
+            this.lunchStart = new System.Windows.Forms.Button();
+            this.lunchEnd = new System.Windows.Forms.Button();
+            this.facultyName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // facultyId
+            // 
+            this.facultyId.Location = new System.Drawing.Point(12, 25);
+            this.facultyId.Name = "facultyId";
+            this.facultyId.Size = new System.Drawing.Size(100, 20);
+            this.facultyId.TabIndex = 2;
+            this.facultyId.TextChanged += new System.EventHandler(this.facultyId_TextChanged);
+            // 
+            // clockIn
+            // 
+            this.clockIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(14)))), ((int)(((byte)(42)))));
+            this.clockIn.ForeColor = System.Drawing.Color.White;
+            this.clockIn.Location = new System.Drawing.Point(121, 63);
+            this.clockIn.Name = "clockIn";
+            this.clockIn.Size = new System.Drawing.Size(88, 37);
+            this.clockIn.TabIndex = 4;
+            this.clockIn.Text = "CLOCK IN";
+            this.clockIn.UseVisualStyleBackColor = false;
+            this.clockIn.Click += new System.EventHandler(this.clockIn_Click);
+            // 
+            // clockOut
+            // 
+            this.clockOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(14)))), ((int)(((byte)(42)))));
+            this.clockOut.ForeColor = System.Drawing.Color.White;
+            this.clockOut.Location = new System.Drawing.Point(121, 164);
+            this.clockOut.Name = "clockOut";
+            this.clockOut.Size = new System.Drawing.Size(88, 37);
+            this.clockOut.TabIndex = 5;
+            this.clockOut.Text = "CLOCK OUT";
+            this.clockOut.UseVisualStyleBackColor = false;
+            this.clockOut.Click += new System.EventHandler(this.clockOut_Click);
+            // 
+            // printLog
+            // 
+            this.printLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.printLog.FormattingEnabled = true;
+            this.printLog.Location = new System.Drawing.Point(12, 218);
+            this.printLog.Name = "printLog";
+            this.printLog.Size = new System.Drawing.Size(327, 121);
+            this.printLog.TabIndex = 8;
+            // 
+            // lunchStart
+            // 
+            this.lunchStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(67)))), ((int)(((byte)(132)))));
+            this.lunchStart.ForeColor = System.Drawing.Color.White;
+            this.lunchStart.Location = new System.Drawing.Point(150, 106);
+            this.lunchStart.Name = "lunchStart";
+            this.lunchStart.Size = new System.Drawing.Size(118, 23);
+            this.lunchStart.TabIndex = 9;
+            this.lunchStart.Text = "OUT TO LUNCH";
+            this.lunchStart.UseVisualStyleBackColor = false;
+            this.lunchStart.Click += new System.EventHandler(this.lunchStart_Click);
+            // 
+            // lunchEnd
+            // 
+            this.lunchEnd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(67)))), ((int)(((byte)(132)))));
+            this.lunchEnd.ForeColor = System.Drawing.Color.White;
+            this.lunchEnd.Location = new System.Drawing.Point(150, 135);
+            this.lunchEnd.Name = "lunchEnd";
+            this.lunchEnd.Size = new System.Drawing.Size(118, 23);
+            this.lunchEnd.TabIndex = 10;
+            this.lunchEnd.Text = "BACK FROM LUNCH";
+            this.lunchEnd.UseVisualStyleBackColor = false;
+            this.lunchEnd.Click += new System.EventHandler(this.lunchEnd_Click);
+            // 
+            // facultyName
+            // 
+            this.facultyName.AutoSize = true;
+            this.facultyName.Location = new System.Drawing.Point(118, 28);
+            this.facultyName.Name = "facultyName";
+            this.facultyName.Size = new System.Drawing.Size(57, 13);
+            this.facultyName.TabIndex = 11;
+            this.facultyName.Text = "Not Found";
+            // 
+            // Punchcard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(352, 344);
+            this.Controls.Add(this.facultyName);
+            this.Controls.Add(this.lunchEnd);
+            this.Controls.Add(this.lunchStart);
+            this.Controls.Add(this.printLog);
+            this.Controls.Add(this.clockOut);
+            this.Controls.Add(this.clockIn);
+            this.Controls.Add(this.facultyId);
+            this.Controls.Add(this.label1);
+            this.Name = "Punchcard";
+            this.Text = "Punchcard";
+            this.Load += new System.EventHandler(this.Punchcard_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox facultyId;
+        private System.Windows.Forms.Button clockIn;
+        private System.Windows.Forms.Button clockOut;
+        private System.Windows.Forms.ListBox printLog;
+        private System.Windows.Forms.Button lunchStart;
+        private System.Windows.Forms.Button lunchEnd;
+        private System.Windows.Forms.Label facultyName;
+    }
+}
+
---
+++ TimeclockTracker/Punchcard.cs
@@ -0,0 +1,132 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TimeclockTracker
+{
+    public partial class Punchcard : Form
+    {
+        // Define request parameters.
+        String spreadsheetId = ConfigurationManager.AppSettings["sheet"];
+        String logSheet = ConfigurationManager.AppSettings["log"];
+        String facultySheet = ConfigurationManager.AppSettings["faculty"];
+        SheetsService service;
+        Dictionary<string, Faculty> facultyData = new Dictionary<string, Faculty>();
+        public Punchcard()
+        {
+            InitializeComponent();
+        }
+
+        private void Punchcard_Load(object sender, EventArgs e)
+        {
+            UserCredential credential;
+
+            // The file token.json stores the user's access and refresh tokens, and is created
+            // automatically when the authorization flow completes for the first time.
+            string credPath = "token.json";
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.FromFile("credentials.json").Secrets,
+                new string[] { "https://www.googleapis.com/auth/spreadsheets" },
+                "user",
+                CancellationToken.None,
+                new FileDataStore(credPath, true)).Result;
+            Console.WriteLine("Credential file saved to: " + credPath);
+            // Create Google Sheets API service.
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Punchcard",
+            });
+
+            var req = service.Spreadsheets.Values.Get(spreadsheetId, facultySheet + "!A:C");
+            var facultyResponse = req.Execute();
+            facultyResponse.Values.RemoveAt(0); // remove header row
+
+            foreach (List<Object> faculty in facultyResponse.Values)
+            {
+                facultyData.Add(faculty[0].ToString(), new Faculty(faculty[0].ToString(), faculty[1].ToString(), faculty[2].ToString()));
+            }
+        }
+
+        private void facultyId_TextChanged(object sender, EventArgs e)
+        {
+            Faculty faculty;
+            facultyData.TryGetValue(facultyId.Text.Trim(), out faculty);
+            facultyName.Text = faculty == null ? "Not Found": faculty.name;
+        }
+
+        private void logRows(String type)
+        {
+            try
+            {
+                var faculty = facultyData[facultyId.Text.Trim()];
+                var time = DateTime.Now;
+
+                var range = new ValueRange()
+                {
+                    Values = new List<IList<object>> {
+                            new List<object> {
+                                time.ToString(), type, faculty.id, faculty.name
+                            }
+                        }
+                };
+
+                var append = service.Spreadsheets.Values.Append(range, spreadsheetId, logSheet + "!A1:A");
+                append.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+                append.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+                var resp = append.Execute();
+                printLog.Items.Add($"{type} {faculty.name} {time}");
+
+                facultyId.Text = "";
+            } catch (Exception e) {
+                printLog.Items.Add($"ERROR: {e.Message}");
+            }
+        }
+
+        private void clockIn_Click(object sender, EventArgs e)
+        {
+            logRows("CLOCK_IN");
+        }
+
+        private void clockOut_Click(object sender, EventArgs e)
+        {
+            logRows("CLOCK_OUT");
+        }
+
+        private void lunchStart_Click(object sender, EventArgs e)
+        {
+            logRows("LUNCH_START");
+        }
+
+        private void lunchEnd_Click(object sender, EventArgs e)
+        {
+            logRows("LUNCH_END");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    internal class Faculty
+    {
+        public Faculty(string id, string last, string first) {
+            this.id = id;
+            this.last = last;
+            this.first = first;
+        }
+        public string id;
+        public string last;
+        public string first;
+
+        public string name { get { return last + ", " + first; } }
+    }
+}
